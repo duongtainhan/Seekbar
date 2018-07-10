89cf5e740de1254c3ce42cdcf52d3431a3bf4322
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-07-10 19:05:01 +0700

    exe

--- app/src/main/java/com/example/duongtainhan555/seekbar/MainActivity.java
+++ app/src/main/java/com/example/duongtainhan555/seekbar/MainActivity.java
@@ -2,12 +2,58 @@ package com.example.duongtainhan555.seekbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SeekBar seekBar1, seekBar2;
+    private int progress1, progress2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Anh xa
+        seekBar1 = findViewById(R.id.seekBar1);
+        seekBar2 = findViewById(R.id.seekBar2);
+        //
+
+        seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                seekBar2.setProgress(i);
+                progress1 = i;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                progress2 = i;
+                if(progress2 < progress1)
+                {
+                    seekBar2.setProgress(progress1);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
